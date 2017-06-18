ad43087d51aeca9cb1e1f6726bde9dc39f9f8bb5
davidallenmann <dm@humu.io>
2017-06-18 08:50:42 -0400

    feather battery library

--- OpenTag2_libraries/Adafruit_SSD1306/examples/ssd1306_128x64_i2c/ssd1306_128x64_i2c.ino
+++ OpenTag2_libraries/Adafruit_SSD1306/examples/ssd1306_128x64_i2c/ssd1306_128x64_i2c.ino
@@ -148,17 +148,19 @@ void setup()   {
   display.print("0x"); display.println(0xDEADBEEF, HEX);
   display.display();
   delay(2000);
+  display.clearDisplay();
 
   // miniature bitmap display
-  display.clearDisplay();
   display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
   display.display();
+  delay(1);
 
   // invert the display
   display.invertDisplay(true);
   delay(1000); 
   display.invertDisplay(false);
   delay(1000); 
+  display.clearDisplay();
 
   // draw a bitmap icon and 'animate' movement
   testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
@@ -190,21 +192,21 @@ void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
   while (1) {
     // draw each icon
     for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], logo16_glcd_bmp, w, h, WHITE);
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, WHITE);
     }
     display.display();
     delay(200);
     
     // then erase it + move it
     for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS],  logo16_glcd_bmp, w, h, BLACK);
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, BLACK);
       // move it
       icons[f][YPOS] += icons[f][DELTAY];
       // if its gone, reinit
       if (icons[f][YPOS] > display.height()) {
-	icons[f][XPOS] = random(display.width());
-	icons[f][YPOS] = 0;
-	icons[f][DELTAY] = random(5) + 1;
+        icons[f][XPOS] = random(display.width());
+        icons[f][YPOS] = 0;
+        icons[f][DELTAY] = random(5) + 1;
       }
     }
    }
@@ -223,12 +225,14 @@ void testdrawchar(void) {
       display.println();
   }    
   display.display();
+  delay(1);
 }
 
 void testdrawcircle(void) {
   for (int16_t i=0; i<display.height(); i+=2) {
     display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
     display.display();
+    delay(1);
   }
 }
 
@@ -238,6 +242,7 @@ void testfillrect(void) {
     // alternate colors
     display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color%2);
     display.display();
+    delay(1);
     color++;
   }
 }
@@ -248,6 +253,7 @@ void testdrawtriangle(void) {
                      display.width()/2-i, display.height()/2+i,
                      display.width()/2+i, display.height()/2+i, WHITE);
     display.display();
+    delay(1);
   }
 }
 
@@ -260,6 +266,7 @@ void testfilltriangle(void) {
     if (color == WHITE) color = BLACK;
     else color = WHITE;
     display.display();
+    delay(1);
   }
 }
 
@@ -267,6 +274,7 @@ void testdrawroundrect(void) {
   for (int16_t i=0; i<display.height()/2-2; i+=2) {
     display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, WHITE);
     display.display();
+    delay(1);
   }
 }
 
@@ -277,6 +285,7 @@ void testfillroundrect(void) {
     if (color == WHITE) color = BLACK;
     else color = WHITE;
     display.display();
+    delay(1);
   }
 }
    
@@ -284,6 +293,7 @@ void testdrawrect(void) {
   for (int16_t i=0; i<display.height()/2; i+=2) {
     display.drawRect(i, i, display.width()-2*i, display.height()-2*i, WHITE);
     display.display();
+    delay(1);
   }
 }
 
@@ -291,10 +301,12 @@ void testdrawline() {
   for (int16_t i=0; i<display.width(); i+=4) {
     display.drawLine(0, 0, i, display.height()-1, WHITE);
     display.display();
+    delay(1);
   }
   for (int16_t i=0; i<display.height(); i+=4) {
     display.drawLine(0, 0, display.width()-1, i, WHITE);
     display.display();
+    delay(1);
   }
   delay(250);
   
@@ -302,10 +314,12 @@ void testdrawline() {
   for (int16_t i=0; i<display.width(); i+=4) {
     display.drawLine(0, display.height()-1, i, 0, WHITE);
     display.display();
+    delay(1);
   }
   for (int16_t i=display.height()-1; i>=0; i-=4) {
     display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
     display.display();
+    delay(1);
   }
   delay(250);
   
@@ -313,10 +327,12 @@ void testdrawline() {
   for (int16_t i=display.width()-1; i>=0; i-=4) {
     display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
     display.display();
+    delay(1);
   }
   for (int16_t i=display.height()-1; i>=0; i-=4) {
     display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
     display.display();
+    delay(1);
   }
   delay(250);
 
@@ -324,10 +340,12 @@ void testdrawline() {
   for (int16_t i=0; i<display.height(); i+=4) {
     display.drawLine(display.width()-1, 0, 0, i, WHITE);
     display.display();
+    delay(1);
   }
   for (int16_t i=0; i<display.width(); i+=4) {
     display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE); 
     display.display();
+    delay(1);
   }
   delay(250);
 }
@@ -339,6 +357,7 @@ void testscrolltext(void) {
   display.clearDisplay();
   display.println("scroll");
   display.display();
+  delay(1);
  
   display.startscrollright(0x00, 0x0F);
   delay(2000);
